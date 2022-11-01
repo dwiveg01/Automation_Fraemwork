ac3daf5af69845ced47992530da71b955d9a6767
gaurav <gaurav.dwivedy94@gmail.com>
2022-11-01 14:19:35 +0530

    Base class and POM implemented.

--- src/test/java/com/automationqa/BaseClass.java
+++ src/test/java/com/automationqa/BaseClass.java
@@ -1,4 +1,30 @@
 package com.automationqa;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
 public class BaseClass {
+
+    public static WebDriver driver;
+    public static String baseUrl="https://amazon.in";
+    public static int waitTimeOut=10;
+
+    @Test
+        public void setup() throws InterruptedException {
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get(baseUrl);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(waitTimeOut));
+            driver.quit();
+        }
+
+
+
+
 }
